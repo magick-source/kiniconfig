e6c12acd641ca32d937f4b987ae70969f377b655
theMage <themage@magicksource.eu>
2025-11-29 03:07:03 +0100

    Improve build process (publish)
    - add support for maven-publish
    - add release plugin
    
    themage

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,17 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
+    signing
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.detekt)
     alias(libs.plugins.kover)
+    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.release)
     `java-library`
 }
 
 group = "eu.magicksource.kiniconfig"
+val artifact = "kiniconfig"
 version = "0.1.0"
 
 tasks.withType<Test> {
@@ -28,4 +32,22 @@ dependencies {
     implementation(libs.konfig)
     testImplementation(kotlin("test"))
     testImplementation(libs.junit.jupiter)
+}
+
+mavenPublishing {
+    coordinates(group.toString(), artifact, version.toString())
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        organization {
+            name.set(findProperty("POM_ORGANIZATION") as String)
+            url.set(findProperty("POM_ORGANIZATION_URL") as String)
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+    useGpgCmd()
 }
